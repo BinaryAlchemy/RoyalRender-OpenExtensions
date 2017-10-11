effeeccaf42798a027590be97e3c0522baa686dc
h-schoenberger <web@BinaryAlchemy.de>
2017-10-11 19:44:25 +0200

    .

--- _plugins/control/170906_WindowsReplaceServiceExecutable/ReplaceServiceFile.py
+++ _plugins/control/170906_WindowsReplaceServiceExecutable/ReplaceServiceFile.py
@@ -2,15 +2,19 @@ import os
 import shutil
 client= rr.getClient()
 
-localpath="\\\\"+client.name()+"\\C$\\Program Files (x86)\\rrService\\rrAutostartservice.exe"
+print("testing "+client.name()+"... ")
+rrGlobal.refreshUI()
+localpath="\\\\"+client.name()+"\\C$\\Program Files\\"
 if (os.path.exists(localpath)):
-    print(localpath)
-    shutil.copyfile("\\\\AIR\\Royal_Render\\bin\\win\\rrAutostartservice.exe",localpath)
-    rrGlobal.refreshUI()
-    
+    print("    ...online... ")
+    localpath="\\\\"+client.name()+"\\C$\\Program Files (x86)\\rrService\\rrAutostartservice.exe"
+    if (os.path.exists(localpath)):
+        print("Replacing  "+localpath)
+        rrGlobal.refreshUI()
+        shutil.copyfile("\\\\AIR\\Royal_Render\\bin\\win\\rrAutostartservice.exe",localpath)
+    localpath="\\\\"+client.name()+"\\C$\\Program Files\\rrService\\rrAutostartservice.exe"
+    if (os.path.exists(localpath)):
+        print("Replacing  "+localpath)
+        rrGlobal.refreshUI()
+        shutil.copyfile("\\\\AIR\\Royal_Render\\bin\\win64\\rrAutostartservice.exe",localpath)
         
-localpath="\\\\"+client.name()+"\\C$\\Program Files\\rrService\\rrAutostartservice.exe"
-if (os.path.exists(localpath)):
-    print(localpath)
-    shutil.copyfile("\\\\AIR\\Royal_Render\\bin\\win64\\rrAutostartservice.exe",localpath)
-    rrGlobal.refreshUI()
