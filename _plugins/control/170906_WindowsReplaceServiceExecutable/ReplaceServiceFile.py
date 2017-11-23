e37fc4d57534f2d319a75f4dc16ddbfbe0b54613
h-schoenberger <web@BinaryAlchemy.de>
2017-11-23 16:08:22 +0100

    .

--- _plugins/control/170906_WindowsReplaceServiceExecutable/ReplaceServiceFile.py
+++ _plugins/control/170906_WindowsReplaceServiceExecutable/ReplaceServiceFile.py
@@ -9,12 +9,17 @@ if (os.path.exists(localpath)):
     print("    ...online... ")
     localpath="\\\\"+client.name()+"\\C$\\Program Files (x86)\\rrService\\rrAutostartservice.exe"
     if (os.path.exists(localpath)):
+        print("Renaming  "+localpath)
+        rrGlobal.refreshUI()
+        shutil.move(localpath,localpath+"_old2")
         print("Replacing  "+localpath)
         rrGlobal.refreshUI()
         shutil.copyfile("\\\\AIR\\Royal_Render\\bin\\win\\rrAutostartservice.exe",localpath)
     localpath="\\\\"+client.name()+"\\C$\\Program Files\\rrService\\rrAutostartservice.exe"
     if (os.path.exists(localpath)):
-        print("Replacing  "+localpath)
+        print("Renaming  "+localpath)
         rrGlobal.refreshUI()
+        shutil.move(localpath,localpath+"_old2")
+        print("Replacing  "+localpath)
         shutil.copyfile("\\\\AIR\\Royal_Render\\bin\\win64\\rrAutostartservice.exe",localpath)
         
