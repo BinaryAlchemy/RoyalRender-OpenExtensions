98aabf440a0d4bdb86c4069056921b7c907cf5ac
pKrime <palucam@gmail.com>
2018-04-30 16:54:05 +0200

    support for movie and custom format output

--- _render_app/Blender_v2/RR8/render_apps/_submitplugins/rrSubmit_Blender_1+.py
+++ _render_app/Blender_v2/RR8/render_apps/_submitplugins/rrSubmit_Blender_1+.py
@@ -46,13 +46,18 @@ class RoyalRender_Submitter(bpy.types.Panel):
         
         renderOut = os.path.dirname(bpy.data.filepath) + scn.render.filepath
         renderOut = renderOut.replace("//", "\\")
-        layout.label("SubmitSceneValues: ")
-        layout.label("StartFrame: " + str(scn.frame_start))
-        layout.label("EndFrame: " + str(scn.frame_end))
-        layout.label("ImageType: " + type)
-        layout.label("ImageName: " + os.path.basename(renderOut))
-        layout.label("RenderDir: " +  os.path.dirname(renderOut))  
-        #layout.label()      
+        
+        layout.label("Submit Scene Values: ")
+        split = layout.split(percentage=0.05)
+        col = split.column()
+        col = split.column()
+        row = col.row()
+        row.label("StartFrame: " + str(scn.frame_start))
+        row.label("EndFrame: " + str(scn.frame_end))
+        col.label("ImageType: " + type)
+        col.label("ImageName: " + os.path.basename(renderOut))
+        col.label("RenderDir: " +  os.path.dirname(renderOut))  
+        
         layout.operator("royalrender.submitscene")
         
 class OBJECT_OT_SubmitScene(bpy.types.Operator):
@@ -116,11 +121,50 @@ class OBJECT_OT_SubmitScene(bpy.types.Operator):
         #scn = bpy.data.scenes["Scene"]
         scn = bpy.context.scene
         type = scn.render.image_settings.file_format
-        rendertarget = type
-        extension = "."+rendertarget.lower()
-        extension=extension.replace("open_", "");
-        extension=extension.replace(".tiff", ".tif");
-        renderOut = os.path.dirname(bpy.data.filepath) + scn.render.filepath
+        rendertarget = type.replace("OPEN_", "")
+        if rendertarget == "FFMPEG":
+            ImageSingleOutputFile = True
+            rendertarget = scn.render.ffmpeg.format
+        elif rendertarget == "JPEG2000":
+            ImageSingleOutputFile = False
+            rendertarget = scn.render.image_settings.jpeg2k_codec
+        else:
+            ImageSingleOutputFile = False
+            rendertarget = rendertarget.replace("TARGA_RAW", "RAWTGA")
+            rendertarget = rendertarget.replace("TARGA", "TGA")
+
+        if rendertarget.endswith("_MULTILAYER"):  # i.e. EXR_MULTILAYER
+            extension, rendertarget = rendertarget.rsplit("_", 1)
+            extension = "." + extension.lower()
+        else:
+            extension = "." + rendertarget.lower()
+        if extension.startswith(".avi"):
+            ImageSingleOutputFile = True
+            extension = ".avi"
+            rendertarget = rendertarget.replace("_", "")
+
+        extension=extension.replace(".tiff", ".tif")
+        extension=extension.replace(".jpeg", ".jpg")
+        
+        extension=extension.replace(".quicktime", ".mov")
+        extension=extension.replace(".flash", ".flv")
+        extension=extension.replace(".mpeg1", ".mpg")
+        extension=extension.replace(".mpeg2", ".dvd")
+        extension=extension.replace(".mpeg4", ".mp4")
+        
+        if scn.render.filepath.startswith("//"):  # relative path
+            renderOut = os.path.dirname(bpy.data.filepath) + scn.render.filepath
+        else:
+            renderOut = scn.render.filepath
+        if renderOut.endswith('#'):
+            outLen = len(renderOut) - 1
+            renderPadding = outLen - next(i for i in range(outLen, 0, -1) if renderOut[i] != '#')
+
+            if ImageSingleOutputFile:
+                renderOut = "{0}{1:0{3}d}-{2:0{3}d}".format(renderOut, scn.frame_start, scn.frame_end, renderPadding)
+        else:
+            renderPadding = 1
+
         renderOut = renderOut.replace("//", "\\")
         
         writeNodeStr = self.writeNodeStr
@@ -132,14 +176,19 @@ class OBJECT_OT_SubmitScene(bpy.types.Operator):
         writeNodeStr(fileID,"Layer", scn.render.layers[0].name)
         writeNodeStr(fileID,"SceneName",bpy.data.filepath)
         writeNodeBool(fileID,"IsActive", True )
+        writeNodeBool(fileID,"ImageSingleOutputFile", ImageSingleOutputFile)
         writeNodeInt(fileID,"SeqStart",scn.frame_start)
         writeNodeInt(fileID,"SeqEnd",scn.frame_end)
         writeNodeInt(fileID,"SeqStep",scn.frame_step)
         #writeNodeStr(fileID,"ImagePreNumberLetter","_")
         writeNodeStr(fileID,"ImageDir",os.path.dirname(renderOut) ) 
         writeNodeStr(fileID,"ImageFilename",os.path.basename(renderOut))
+        writeNodeInt(fileID,"ImageFramePadding", renderPadding)
         writeNodeStr(fileID,"ImageExtension",extension)
-        writeNodeStr(fileID,"CustomFrameFormat", rendertarget)
+        if rendertarget in ("TGA", "RAWTGA", "JPEG", "IRIS", "IRIZ", "AVIRAW",
+                            "AVIJPEG", "PNG", "BMP", "HDR", "TIFF", "EXR", "MULTILAYER",
+                            "MPEG", "FRAMESERVER", "CINEON", "DPX", "DDS", "JP2"):
+            writeNodeStr(fileID,"CustomFrameFormat", rendertarget)
         fileID.write("</Job>\n")
         fileID.write("</RR_Job_File>\n")
         fileID.close()
